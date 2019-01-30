c747c7ce4abef1404f9c41c3e7cbc00723600f43
Lou Moore <lourmoore@gmail.com>
2019-01-29 16:34:12 -0800

    myths n facts

---
+++ _site/js/myths.js
@@ -0,0 +1,20 @@
+class Myths {
+  constructor() {
+
+    var outerThis = this;
+    $('div.myth-expander').click(function() {
+      var mythId = this.id.split('-')[2];
+      outerThis.toggleMythDetail(mythId);
+    });
+  }
+
+  toggleMythDetail(mythId) {
+    $('#myth-expander-'+mythId).toggleClass('expanded');
+    if($('#myth-detail-'+mythId).is(":hidden")) {
+      $('#myth-detail-'+mythId).slideDown();
+    } else {
+      $('#myth-detail-'+mythId).slideUp();
+    }
+  };
+
+}
